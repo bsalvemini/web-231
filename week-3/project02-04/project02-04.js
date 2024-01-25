56028411847257c63330b0d334de4679be0795d9
bsalvemini <bcsalvemini@gmail.com>
2024-01-25 09:49:50 -0500

    Add week-3 folder and project02-04 folder, add work for assignment 3.2
    Added the week-3 folder and the project02-04 folder within in it. The work for Assignment 3.2 was completed in the project02-04 folder.

---
+++ week-3/project02-04/project02-04.js
@@ -0,0 +1,73 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-04
+
+      Application to calculate the cost of a restaurant order plus tax
+      Author: Brandon Salvemini
+      Date:   1/23/2024
+
+      Filename: project02-04.js
+ */
+
+// Declare constants for the application
+const CHICKEN_PRICE = 10.95;
+const HALIBUT_PRICE = 13.95;
+const BURGER_PRICE = 9.95;
+const SALMON_PRICE = 18.95;
+const SALAD_PRICE = 7.95;
+const SALES_TAX = 0.07;
+
+// Add click event handlers for each checkbox
+document.getElementById("chicken").onclick = calcTotal;
+document.getElementById("halibut").onclick = calcTotal;
+document.getElementById("burger").onclick = calcTotal;
+document.getElementById("salmon").onclick = calcTotal;
+document.getElementById("salad").onclick = calcTotal;
+
+// Function to calculate the totals and display them on the page
+function calcTotal() {
+  // Declare variable for cost
+  let cost = 0;
+
+  // Declare variables for checked state of each check box
+  let buyChicken = document.getElementById("chicken").checked;
+  let buyHalibut = document.getElementById("halibut").checked;
+  let buyBurger = document.getElementById("burger").checked;
+  let buySalmon = document.getElementById("salmon").checked;
+  let buySalad = document.getElementById("salad").checked;
+
+  // Add the cost of chicken if purchased
+  cost += buyChicken ? CHICKEN_PRICE : 0;
+
+  // Add the cost of halibut if purchased
+  cost += buyHalibut ? HALIBUT_PRICE : 0;
+
+  // Add the cost of burger if purchased
+  cost += buyBurger ? BURGER_PRICE : 0;
+
+  // Add the cost of salmon if purchased
+  cost += buySalmon ? SALMON_PRICE : 0;
+
+  // Add the cost of salad if purchased
+  cost += buySalad ? SALAD_PRICE : 0;
+
+  // Display the food total
+  document.getElementById("foodTotal").innerHTML = formatCurrency(cost);
+
+  // Calculate sales tax
+  let tax = cost * SALES_TAX;
+
+  // Display the tax
+  document.getElementById("foodTax").innerHTML = formatCurrency(tax);
+
+  // Calculate the total cost
+  let totalCost = cost + tax;
+
+  // Display the total cost
+  document.getElementById("totalBill").innerHTML = formatCurrency(totalCost);
+}
+
+// Function to display a numeric value as a text string in the format $##.##
+function formatCurrency(value) {
+  return "$" + value.toFixed(2);
+}
